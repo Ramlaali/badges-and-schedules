2a0a2367bba66cb726cccaaef7b7341268d6dc40
Ramla <ramlaali.abd@gmail.com>
2021-03-02 15:25:35 -0500

    commit tree 6ab6ae19ceacdaf86aaa57b4941b0829b341b0d5

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+def badge_maker(name)
+   return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+    speaker = []
+    speakers.each{|name| speaker << badge_maker(name) }
+    return speaker
+end
+
+def assign_rooms(speakers)
+    speaker = []
+    speakers.each_with_index do|name,index|
+        speaker << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+    return speaker
+end
+
+def printer(speakers)
+   batch_badge_creator(speakers).each {|name|puts name} 
+   assign_rooms(speakers).each {|room| puts room}
+end
